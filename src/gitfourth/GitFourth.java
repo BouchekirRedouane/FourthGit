9e0c1b07d24893c96ab15f430b40eea32c7c6cff
redOne <redouane.bouchekir@gmail.com>
2022-02-03 17:08:14 +0100

    First commit

---
+++ src/gitfourth/GitFourth.java
@@ -0,0 +1,12 @@
+package gitfourth;
+
+public class GitFourth {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
